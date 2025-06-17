fca93db816e2a82675d8f13009f4008af99c17bd
flor3z <46878418+flor3z-github@users.noreply.github.com>
2025-06-17 13:27:59 +0900

    Fix Kakao client ID env variable

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,2 +1,2 @@
-export const KAKAO_CLIENT_ID = process.env.KAKAO_CLIENT_ID ?? "";
+export const KAKAO_CLIENT_ID = import.meta.env.BUN_PUBLIC_KAKAO_CLIENT_ID ?? "";
 export const KAKAO_REDIRECT_URI = "http://localhost:5500/callback";
